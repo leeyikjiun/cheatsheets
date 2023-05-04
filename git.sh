6e7aab178f21eff7cf2da72246fb92f366420813
Lee Yik Jiun <lee.yikjiun@gmail.com>
2023-05-04 11:58:56 +0800

    add git clone with alternate identity

---
+++ git.sh
@@ -0,0 +1,2 @@
+# Clone a repository and set ssh command, user name and email. 
+git clone --config "core.sshcommand=ssh -i <identity_file>" --config "user.name=<user.name>" --config "user.email=<user.email>" <repository>
