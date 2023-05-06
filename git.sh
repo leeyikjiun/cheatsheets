eb6af72c8a2116846bedbb82b341a22c8b4f0d3e
Lee Yik Jiun <lee.yikjiun@gmail.com>
2023-05-06 12:56:28 +0800

    add rewrite committer as author

--- git.sh
+++ git.sh
@@ -1,2 +1,5 @@
 # Clone a repository and set ssh command, user name and email. 
 git clone --config "core.sshcommand=ssh -i <identity_file>" --config "user.name=<user.name>" --config "user.email=<user.email>" <repository>
+
+# Rewrite committer as author.
+GIT_COMMITTER_NAME=$(git log -n 1 --format=%an) GIT_COMMITTER_EMAIL=$(git log -n 1 --format=%ae) GIT_COMMITTER_DATE=$(git log -n 1 --format=%aD) git commit --amend --no-edit
